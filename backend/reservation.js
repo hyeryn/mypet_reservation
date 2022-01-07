17e22fe8f71eb2c94e0f8e11e0c9a5d253583b7e
hyeonji00 <50583561+hyeonji00@users.noreply.github.com>
2022-01-07 21:36:31 +0900

    Update reservation.js
    reservation API 수정

--- backend/reservation.js
+++ backend/reservation.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const mysql = require('mysql');
+const router = require('express').Router();
+var con = require('./db.js');
 
-router.get('/reservation', function(req, res){ // 예약 페이지 주소로 수정
-    con.query(`INSERT INTO reservation (date) VALUES (\'${date}\');`, (err) => {
-        if (err) {
-            console.log('reservation save fail')
-        }
-        else {
-            console.log('reservation save success')
-        }
+router.post('/reservation', function (req, res) { // 하위 링크 수정
+    var name = req.body.name;
+    var mobile = req.body.mobile;
+    var addr = req.body.addr;
+    var time = req.body.time;
+    var type = req.body.type;
+
+    var user_id = req.body.user_id;
+    var date = new Date(req.body.date);
+    var svc_id = req.body.svc_id;
+
+    con.query(`INSERT INTO service (name, mobile, addr, time, type) VALUES (\'${name}\', \'${mobile}\',\'${addr}\',\'${time}'\,\'${type}'\);`, (err) => {
+        if (err) res.json({ success: false, msg: 'service save fail' });
+        else res.json({ success: true, msg: 'service save success' });
     });
-})
+
+    con.query(`INSERT INTO reservation (date, svc_id, client_id) VALUES (\'${date}\', \'${svc_id}\',\'${user_id}\');`, (err) => {
+        if (err) res.json({ success: false, msg: 'res fail' });
+        else res.json({ success: true, msg: 'res success' });
+    });
+});
