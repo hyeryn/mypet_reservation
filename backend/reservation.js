79d4f95f669e1639aa049ff6e1da629c6fc7697a
hyeonji00 <50583561+hyeonji00@users.noreply.github.com>
2022-01-08 14:15:25 +0900

    Update reservation.js
    구조 변경

--- backend/reservation.js
+++ backend/reservation.js
@@ -14,13 +14,14 @@ router.post('/reservation', function (req, res) { // 하위 링크 수정
 
     con.query(`INSERT INTO service (name, mobile, addr, time, type) VALUES (\'${name}\', \'${mobile}\',\'${addr}\',\'${time}'\,\'${type}'\);`, function(err, result1, field) {
         if (err) res.json({ success: false, msg: 'service save fail' });
-        else res.json({ success: true, msg: 'service save success' });
-        tmp_service = result1.insertId;
-    });
+        else{
+            res.json({ success: true, msg: 'service save success' });
+            tmp_service = result1.insertId;
 
-    con.query(`INSERT INTO reservation (date, svc_id, client_id) VALUES (\'${date}\', \'${tmp_service}\',\'${user_id}\');`, (err) => {
-
-        if (err) res.json({ success: false, msg: 'res fail' });
-        else res.json({ success: true, msg: 'res success' });
-    });
+            con.query(`INSERT INTO reservation (date, svc_id, client_id) VALUES (\'${date}\', \'${tmp_service}\',\'${user_id}\');`, (err) => {
+                if (err) res.json({ success: false, msg: 'res fail' });
+                else res.json({ success: true, msg: 'res success' });
+            });
+        }
+    });    
 });
